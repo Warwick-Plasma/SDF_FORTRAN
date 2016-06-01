5afc2c4f17a2c1362760662ec9884a7b3864964e
Keith Bennett <K.Bennett@warwick.ac.uk>
2016-06-01 17:08:16 +0100

    Partial revert of commit 25baad3

--- CMake/CheckMPIVersion.cmake
+++ CMake/CheckMPIVersion.cmake
@@ -3,14 +3,14 @@
 
 try_run(
     MPI_VERSION_RAN MPI_VERSION_COMPILED ${CMAKE_CURRENT_BINARY_DIR}
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/mpi_version.F90
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/mpi_version.f90
     CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${MPI_Fortran_INCLUDE_PATH}"
     LINK_LIBRARIES ${MPI_Fortran_LIBRARIES}
     RUN_OUTPUT_VARIABLE MPI_VERSION
     COMPILE_OUTPUT_VARIABLE COMP)
 if(NOT MPI_VERSION_COMPILED)
     message("${COMP}")
-    message(FATAL_ERROR "Could not compile mpi_version.F90")
+    message(FATAL_ERROR "Could not compile mpi_version.f90")
 endif()
 if(${MPI_VERSION_RAN} STREQUAL FAILED_TO_RUN)
     message(FATAL_ERROR "Could not run mpi_version")
